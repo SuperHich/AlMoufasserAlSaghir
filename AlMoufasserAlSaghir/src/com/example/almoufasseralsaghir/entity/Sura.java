841885251df6444a954672c222336f8becce17ea
RamiTrabelsi <trabelsi.med.rami@gmail.com>
2014-05-22 00:47:53 +0100

    Object Sura

---
+++ AlMoufasserAlSaghir/src/com/example/almoufasseralsaghir/entity/Sura.java
@@ -0,0 +1,36 @@
+package com.example.almoufasseralsaghir.entity;
+
+public class Sura {
+
+	private String db_name; 
+	private String label;
+	private int smallDrawableId ;
+	private int bigDrawableId ;
+	
+	public String getDb_name() {
+		return db_name;
+	}
+	public void setDb_name(String db_name) {
+		this.db_name = db_name;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public int getSmallDrawableId() {
+		return smallDrawableId;
+	}
+	public void setSmallDrawableId(int smallDrawableId) {
+		this.smallDrawableId = smallDrawableId;
+	}
+	public int getBigDrawableId() {
+		return bigDrawableId;
+	}
+	public void setBigDrawableId(int bigDrawableId) {
+		this.bigDrawableId = bigDrawableId;
+	}
+	
+	
+}
