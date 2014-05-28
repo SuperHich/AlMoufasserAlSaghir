761c43452ef1369aba0c88ce210457de4d99699c
RamiTrabelsi <trabelsi.med.rami@gmail.com>
2014-05-28 12:37:34 +0100

    questions containers complete

--- AlMoufasserAlSaghir/src/com/example/almoufasseralsaghir/QuestionsActivity.java
+++ AlMoufasserAlSaghir/src/com/example/almoufasseralsaghir/QuestionsActivity.java
@@ -1,18 +1,19 @@
 package com.example.almoufasseralsaghir;
 
-import com.almoufasseralsaghir.utils.FontFitTextView;
-import com.almoufasseralsaghir.utils.MySuperScaler;
-
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import com.almoufasseralsaghir.utils.FontFitTextView;
+import com.almoufasseralsaghir.utils.MySuperScaler;
 
 public class QuestionsActivity extends MySuperScaler {
 
@@ -23,11 +24,16 @@ public class QuestionsActivity extends MySuperScaler {
 	private ImageView result_3_format_3, result_2_format_3, result_1_format_3,
 					   result_4_format_4, result_3_format_4, result_2_format_4, result_1_format_4 ;
 	
+	private int answers_nbr = 0 ;
+	private boolean format_4 = false;
+	private boolean format_3 = false;
+	private boolean answer ;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.questions_screen);
+		
 		
 		info = (Button) findViewById(R.id.questions_info);
 		favourites = (Button) findViewById(R.id.questions_favourites);
@@ -58,12 +64,77 @@ public class QuestionsActivity extends MySuperScaler {
 		result_1_format_4 = (ImageView) findViewById(R.id.result_1_format_4);
 		
 		
+		selectformat();
 		
-		
-		
-		
-		
-		
+		answer_1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				 if (format_3){
+			    	  if (answers_nbr <3) 
+			    	  {
+			    	   answers_nbr++ ;
+			    	  answer = false ;
+			    	  indicatePlay(answer);
+			    	  }
+		    	  }
+		    	  if (format_4){
+			    	  if (answers_nbr <4) 
+			    	  {
+			    	   answers_nbr++ ;
+			    	  answer = false ;
+			    	  indicatePlay(answer);
+			    	  }
+			       }	
+				
+			}
+		});
+		answer_2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				 if (format_3){
+			    	  if (answers_nbr <3) 
+			    	  {
+			    	   answers_nbr++ ;
+			    	  answer = false ;
+			    	  indicatePlay(answer);
+			    	  }
+		    	  }
+		    	  if (format_4){
+			    	  if (answers_nbr <4) 
+			    	  {
+			    	   answers_nbr++ ;
+			    	  answer = false ;
+			    	  indicatePlay(answer);
+			    	  }
+			       }
+				
+			}
+		});
+		answer_3.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				if (format_3){
+			    	  if (answers_nbr <3) 
+			    	  {
+			    	   answers_nbr++ ;
+			    	  answer = true ;
+			    	  indicatePlay(answer);
+			    	  }
+		    	  }
+		    	  if (format_4){
+			    	  if (answers_nbr <4) 
+			    	  {
+			    	   answers_nbr++ ;
+			    	  answer = true ;
+			    	  indicatePlay(answer);
+			    	  }
+			       }	
+				
+			}
+		});
 		
 		
 		
@@ -174,6 +245,57 @@ public class QuestionsActivity extends MySuperScaler {
 		    }
 		});
 		
+	}
+	
+	private void indicatePlay(boolean answer){
+		
+		if (format_4){
+			switch (answers_nbr){
+			case 1 : if (answer) result_1_format_4.setBackgroundResource(R.drawable.answer_correct);
+						else  result_1_format_4.setBackgroundResource(R.drawable.answer_false);
+				break;
+			case 2 : if (answer) result_2_format_4.setBackgroundResource(R.drawable.answer_correct);
+						else  result_2_format_4.setBackgroundResource(R.drawable.answer_false);
+				break;
+			case 3 : if (answer) result_3_format_4.setBackgroundResource(R.drawable.answer_correct);
+						else  result_3_format_4.setBackgroundResource(R.drawable.answer_false);
+				break ;
+			case 4 : if (answer) result_4_format_4.setBackgroundResource(R.drawable.answer_correct);
+						else  result_4_format_4.setBackgroundResource(R.drawable.answer_false);
+				break ;
+			
+			}
+		}
+		if (format_3){
+			switch (answers_nbr){
+			case 1 : if (answer) result_1_format_3.setBackgroundResource(R.drawable.answer_correct);
+			else  result_1_format_3.setBackgroundResource(R.drawable.answer_false);
+				break;
+			case 2 : if (answer) result_2_format_3.setBackgroundResource(R.drawable.answer_correct);
+						else  result_2_format_3.setBackgroundResource(R.drawable.answer_false);
+				break;
+			case 3 :if (answer) result_3_format_3.setBackgroundResource(R.drawable.answer_correct);
+						else  result_3_format_3.setBackgroundResource(R.drawable.answer_false);
+				break ;
+		}
+		}
+		
+	}
+	
+	private void selectformat(){
+		
+	//	if( format 3)
+		myQuestionsBackground.setBackgroundResource(R.drawable.questions_bg_format_3);
+		format_3 = true ; format_4 = false ;
+		results_format_3.setVisibility(View.VISIBLE);
+		results_format_4.setVisibility(View.GONE);
+	
+		//	else	
+		
+//		myQuestionsBackground.setBackgroundResource(R.drawable.questions_bg_format_4);
+//		format_4 = true ; format_3 = false ;
+//		results_format_3.setVisibility(View.GONE);
+//		results_format_4.setVisibility(View.VISIBLE);
 	}
 	
 }
